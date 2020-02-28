95a71d2933a84dfb1f4c6b7f00f6c4e344b4cab7
Muna Chimoriya <muna.chimoriya.2022@student.lhschools.org>
2020-02-28 12:19:59 -0500

    intellij Commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,135 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Phase One
+        String nounDescribingEmotion;
+        String focalBodyPart ;
+        String adverb;
+        String verb;
+        String place;
+        String location;
+        String verbMovement;
+        String verbDescribingBadDeed;
+        String objectNature;
+        String clothing;
+        String positiveWord;
+        String partOfBody;
+        String temperature;
+        String liquid;
+        String presentVerb;
+        String verbAction;
+        String somethingCircular;
+        String adVerbEndinInLy;
+        String nounHousholdObject ;
+        String thingsThatAreold;
+        Scanner keyboard;
+
+            //Phase Two
+              keyboard = new Scanner(System.in);
+
+              System.out.println("Give me a place");
+              place = keyboard.nextLine();
+
+              System.out.println("Give me a focal body part");
+             focalBodyPart = keyboard.nextLine();
+
+            System.out.println("Give me an adverb ");
+            adverb = keyboard.nextLine();
+
+            System.out.println("Give me a noun that describes emotion");
+            nounDescribingEmotion = keyboard.nextLine();
+
+            System.out.println("Give me a verb relating to movement");
+            verbMovement = keyboard.nextLine();
+
+            System.out.println ("Give me a verb");
+            verb = keyboard.nextLine();
+
+            System.out.println ("Name a verb that describes a bad deed");
+            verbDescribingBadDeed = keyboard.nextLine();
+
+            System.out.println ("Name a object from nature ");
+            objectNature = keyboard.nextLine();
+
+            System.out.println ("Name an article of clothing");
+            clothing = keyboard.nextLine();
+
+            System.out.println ("Give me a positive noun");
+            positiveWord = keyboard.nextLine();
+
+            System.out.println ("Name a part of body");
+            partOfBody = keyboard.nextLine();
+
+            System.out.println ("Name a type of liquid");
+            liquid = keyboard.nextLine();
+
+            System.out.println ("Give me a noun that describes temperature");
+            temperature = keyboard.nextLine();
+
+            System.out.println ("Give me a present verb");
+            presentVerb = keyboard.nextLine();
+
+            System.out.println ("Give me an action verb");
+            verbAction= keyboard.nextLine();
+
+            System.out.println ("Name a household object");
+            nounHousholdObject= keyboard.nextLine();
+
+            System.out.println ("Name something circular");
+            somethingCircular= keyboard.nextLine();
+
+            System.out.println ("Give me a location");
+            location= keyboard.nextLine();
+
+            System.out.println ("Name something old ");
+             thingsThatAreold= keyboard.nextLine();
+
+            System.out.println ("Give me adverb ending in -ly");
+            adVerbEndinInLy= keyboard.nextLine();
+
+
+
+        //Phase three
+        System.out.println(" I got the "+ nounDescribingEmotion +" inside my " + focalBodyPart);
+        System.out.println("It goes "+ adverb);
+        System.out.println(",wavy when I "+ verb);
+        System.out.println("it on, all through my "+ place);
+        System.out.println("all through my "+ location);
+        System.out.println("We're " + verbMovement);
+        System.out.println("up,no "+ verbDescribingBadDeed);
+        System.out.println("when we in our zone, I got that "+objectNature);
+        System.out.println("in my "+clothing);
+        System.out.println("got that "+positiveWord);
+        System.out.println("soul in my "+ partOfBody);
+        System.out.println("I feel that  "  + temperature  +  liquid);
+        System.out.println("in my body when when it " + presentVerb);
+        System.out.println("I can't take my "+ somethingCircular);
+        System.out.println("up off it, "+ verbAction);
+        System.out.println("so "+ adVerbEndinInLy );
+        System.out.println("Room on "+ nounHousholdObject);
+        System.out.println("the way we "+ thingsThatAreold);
+        System.out.println(", so don't stop");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
